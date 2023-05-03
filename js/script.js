13a8796be46cb4e0e0547b39dfda9f223b5ef6d7
Izy Dempsey <117126818+izydempsey@users.noreply.github.com>
2023-05-03 19:10:11 -0400

    Used callback functions to make a subscription calculator

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
-var results = document.querySelector(".result");
+var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
@@ -10,5 +10,24 @@ var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
   subType = e.target.value;
-  console.log(subType);
+  //console.log(subType);
+  updateSubscriptionDiv();
 });
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  //console.log(subDuration);
+  updateSubscriptionDiv();
+});
+//ok i committed the vs code version to git hub so im gonna go work there from now on. Go find it if u  wanna see the rest of this story~
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `You have selected a ${subDuration} month plan for $${total}. Tight.`;
+};
